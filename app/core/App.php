a11d197cd864353f547a17233407bd06f4aa5d81
Taufik Herjanto <taufik.herjanto@live.com>
2024-01-19 23:19:26 +0700

    Add routing

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,23 @@
 class App {
     public function __construct()
     {
-        echo 'Ok!';
+        $url = $this->parseUrl();
+        var_dump($url);
+    }
+
+    public function parseUrl()
+    {
+        if ( isset($_GET) ) {
+            // right trim back slash
+            $url = trim($_GET['url'], '/');
+
+            // filter url
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+
+            //explode url to array
+            $url = explode('/', $url);
+
+            return $url;
+        }
     }
 }
