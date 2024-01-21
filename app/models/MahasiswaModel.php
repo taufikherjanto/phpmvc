3f1add33ec8a7582834155224aadf59f420d9073
Taufik Herjanto <taufik.herjanto@live.com>
2024-01-21 22:28:11 +0700

    Add db

--- app/models/MahasiswaModel.php
+++ app/models/MahasiswaModel.php
@@ -1,6 +1,7 @@
 <?php
 
 class MahasiswaModel {
+    /*
     private $mhs = [
         [
             'nama'  => 'Taufik',
@@ -18,9 +19,28 @@ class MahasiswaModel {
             'jurusan'   => 'sistem informasi'
         ]
     ];
+    */
+
+    private $dbh; //database handler
+    private $stmt; //statement
+
+    public function __construct()
+    {
+        // data source name
+        $dsn = 'mysql:host=localhost;dbname=phpmvc';
+
+        try {
+            $this->dbh = new PDO($dsn, 'root', '');
+        } catch(PDOException $e) {
+            die($e->getMessage());
+        }
+    }
 
     public function getAllMahasiswa()
     {
-        return $this->mhs;
+        //return $this->mhs;
+        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+        $this->stmt->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
